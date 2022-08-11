37f8bc254c237e6608ac6476cbe1148d722383d5
Andy Maleh <andy.am@gmail.com>
2022-08-11 18:37:24 -0400

    Initial in-memory bookmarking support

--- lib/rubio/station.rb
+++ lib/rubio/station.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
 module Rubio
-  Station = Struct.new(:stationuuid, :name, :language, :url, :play) do
-    attr_reader :playing
+  Station = Struct.new(:stationuuid, :name, :language, :url, :play, :bookmark) do
+    attr_reader :playing, :bookmarked
 
     def initialize(*args, **kwargs)
       super(*args, **kwargs)
       self.playing = false
+      self.bookmarked = false
     end
 
     def playing=(value)
       self.play = value ? '■' : '▶'
       @playing = value
+    end
+
+    def bookmarked=(value)
+      self.bookmark = value ? '★' : '☆'
+      @bookmarked = value
     end
   end
 end
